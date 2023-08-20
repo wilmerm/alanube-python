bd33466cfdfc4279a2a91f6540dd04909c23c4c3
wilmerm <wilmermorelmartinez@gmail.com>
2023-08-20 05:35:32 -0400

    New parameters are added to the setup

--- setup.py
+++ setup.py
@@ -1,43 +1,50 @@
 from setuptools import setup, find_packages
 
 
-# Metadatos del proyecto
-name = "alanube"
-version = "0.0.0"
-author = "Wilmer Martinez"
-description = "Library to establish connections with the Alanube API."
-url = "https://github.com/wilmerm/alanube-python"
-license = "MIT"
-
-# Archivos que se incluirán en la distribución
+# Files to be included in the distribution
 packages = find_packages()
 
-# Dependencias (si las hay)
+# Dependences
 install_requires = [
     'requests',
 ]
 
-# Configuración del setup
+# Setup configuration
 setup(
-    name=name,
-    version=version,
-    author=author,
-    description=description,
-    url=url,
-    license=license,
+    name='alanube',
+    version='0.0.2',
+    author='Wilmer Martinez',
+    author_email='info@wilmermartinez.dev',
+    description='Library to establish connections with the Alanube API.',
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/wilmerm/alanube-python',
+    license='MIT',
     packages=packages,
     install_requires=install_requires,
+    keywords=[
+        'alanube',
+        'api',
+        'dgii',
+        'ncf',
+        'electronic billing',
+        'electronic bill',
+        'billing',
+        'bill',
+        'invoice',
+        'erp',
+    ],
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
