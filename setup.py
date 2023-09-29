978b49a2a53f1d89f0fbd78441a91771a7172c47
Wilmer Martinez <info@wilmermartinez.dev>
2023-09-29 04:50:32 -0400

    Se actualiza información de la versión de 0.0.3 a 0.0.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_requires = [
 # Setup configuration
 setup(
     name='alanube',
-    version='0.0.3',
+    version='0.0.4',
     author='Wilmer Martinez',
     author_email='info@wilmermartinez.dev',
     description='Library to establish connections with the Alanube API.',
