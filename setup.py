626cd0058f7ea0150bfdcc3c301e2dd656060dda
Wilmer Martinez <info@wilmermartinez.dev>
2023-09-30 18:50:24 -0400

    Nueva versión que incluye el logo de la DGII

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_requires = [
 # Setup configuration
 setup(
     name='alanube',
-    version='0.0.7',
+    version='0.0.8',
     author='Wilmer Martinez',
     author_email='info@wilmermartinez.dev',
     description='Library to establish connections with the Alanube API.',
